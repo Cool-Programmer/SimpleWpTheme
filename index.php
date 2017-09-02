eb3dfd36b84a8b4c4354e5d6bfd2527a5e29af43
The Cyool Programmer <iodevllc@gmail.com>
2017-09-02 06:09:40 -0700

    Finished structure, added basic styling

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
-<!DOCTYPE html>
-<html <?php echo language_attributes(); ?>>
-<head>
-	<meta charset="<?php echo bloginfo('charset'); ?>">
-	<title><?php echo bloginfo('name'); ?></title>
-	<link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url'); ?>">
-	<?php wp_head(); ?>
-</head>
-<body>
-	
-</body>
-</html>
+<?php get_header(); ?>
+<div class="main">
+	<div class="container">
+		<?php if(have_posts()): ?>
+			<?php while(have_posts()): the_post(); ?>
+				<h3> <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> </h3>
+				<div class="meta">
+					Created by <?php the_author(); ?>
+					on <?php the_time('F j, Y g:i A'); ?>
+				</div>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+		<?php else: ?>
+			<h3><?php echo wpautop("No posts were found...") ?></h3>
+		<?php endif; ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
