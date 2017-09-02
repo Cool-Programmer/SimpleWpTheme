82637222e0971b3377d4f871e7033a3993408a19
The Cyool Programmer <iodevllc@gmail.com>
2017-09-02 05:35:50 -0700

    Initial commit. Setup the directories and linked stylesheets

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php echo language_attributes(); ?>>
+<head>
+	<meta charset="<?php echo bloginfo('charset'); ?>">
+	<title><?php echo bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url'); ?>">
+	<?php wp_head(); ?>
+</head>
+<body>
+	
+</body>
+</html>
